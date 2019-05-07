0938e4a1a7f54a2a4f2a07ca1a6ec76ae08e61f4
Andrew Pendleton <andrew@mapbox.com>
2019-05-06 23:26:00 -0400

    cargo fmt

--- rust-src/src/gridstore/store.rs
+++ rust-src/src/gridstore/store.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 use byteorder::{LittleEndian, ReadBytesExt};
 use flatbuffers;
 use morton::deinterleave_morton;
-use rocksdb::{Direction, IteratorMode, DB, Options};
+use rocksdb::{Direction, IteratorMode, Options, DB};
 
 use crate::gridstore::common::*;
 use crate::gridstore::gridstore_generated::*;
