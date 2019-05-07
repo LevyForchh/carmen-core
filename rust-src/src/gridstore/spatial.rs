648a51ccdffc1101d3eec109202afc299a000cec
aarthykc <aarthy@mapbox.com>
2019-05-07 17:35:24 -0400

    return a slice of coords found through binary search

--- rust-src/src/gridstore/spatial.rs
+++ rust-src/src/gridstore/spatial.rs
@@ -1,31 +1,34 @@
 use flatbuffers;
 use crate::gridstore::gridstore_generated::*;
 use morton::interleave_morton;
-use std::cmp::Ordering::{self, Less, Equal, Greater};
+use std::cmp::Ordering::{Less, Equal, Greater};
 
-fn bbox_filter<'a>(coords: flatbuffers::Vector<'a, flatbuffers::ForwardsUOffset<Coord>>, bbox: (u16, u16, u16, u16)) -> Result<usize, usize>
-{
+fn bbox_filter<'a>(coords: &'a flatbuffers::Vector<'a, flatbuffers::ForwardsUOffset<Coord>>, bbox: (u16, u16, u16, u16)) -> impl Iterator<Item=Coord<'a>> {
     let min = interleave_morton(bbox.0, bbox.1);
-    let _max = interleave_morton(bbox.2, bbox.3);
+    let max = interleave_morton(bbox.2, bbox.3);
+    let start = bbox_binary_search(&coords, min, 0).unwrap() as u32;
+    let end = bbox_binary_search(&coords, max, start).unwrap() as u32;
+    (start..end).map(move |idx| coords.get(idx as usize))
+}
 
+fn bbox_binary_search(coords: &flatbuffers::Vector<flatbuffers::ForwardsUOffset<Coord>>, pos: u32, offset: u32) -> Result<usize, usize> {
     let mut size = coords.len();
 
     // untested
     if size == 0 {
         return Err(0);
-    } 
+    }
 
-    let mut base = 0usize;
+    let mut base = offset as usize;
     while size > 1 {
         let half = size / 2;
         let mid = base + half;
-
         let v = coords.get(mid).coord();
-        let cmp = v.cmp(&min);
+        let cmp = v.cmp(&pos);
         base = if cmp == Greater { base } else { mid };
         size -= half;
     }
-    let cmp = coords.get(base).coord().cmp(&min);
+    let cmp = coords.get(base).coord().cmp(&pos);
     if cmp == Equal { Ok(base) } else { Err(base + (cmp == Less) as usize ) }
 }
 
@@ -38,11 +41,11 @@ mod test {
         let mut fb_builder = flatbuffers::FlatBufferBuilder::new_with_capacity(256);
         {
             let mut coords: Vec<_> = Vec::new();
-            {
-                let ids: Vec<u32> = vec![0];
+            let ids: Vec<u32> = vec![0; 4];
+            for i in 0..ids.len() {
                 let fb_ids = fb_builder.create_vector(&ids);
                 let fb_coord = Coord::create(&mut fb_builder, &CoordArgs{
-                    coord: 1,
+                    coord: i as u32,
                     ids: Some(fb_ids)
                 });
                 coords.push(fb_coord);
@@ -60,7 +63,6 @@ mod test {
         let rs = flatbuffers::get_root::<RelevScore>(&data);
 
         let coords = rs.coords().unwrap();
-
-        let _result = bbox_filter(coords, (0,0,1,1));
+        let result = bbox_filter(&coords, (0,0,1,1)).collect::<Vec<Coord>>();
     }
 }
