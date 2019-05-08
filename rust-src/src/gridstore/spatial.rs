27be5880705dd236ef4bcc028a2f98bec4f9d4ab
aarthykc <aarthy@mapbox.com>
2019-05-08 10:56:59 -0400

    change usize to u32

--- rust-src/src/gridstore/spatial.rs
+++ rust-src/src/gridstore/spatial.rs
@@ -3,33 +3,33 @@ use crate::gridstore::gridstore_generated::*;
 use morton::interleave_morton;
 use std::cmp::Ordering::{Less, Equal, Greater};
 
-fn bbox_filter<'a>(coords: &'a flatbuffers::Vector<'a, flatbuffers::ForwardsUOffset<Coord>>, bbox: (u16, u16, u16, u16)) -> impl Iterator<Item=Coord<'a>> {
-    let min = interleave_morton(bbox.0, bbox.1);
-    let max = interleave_morton(bbox.2, bbox.3);
-    let start = bbox_binary_search(&coords, min, 0).unwrap() as u32;
-    let end = bbox_binary_search(&coords, max, start).unwrap() as u32;
+fn bbox_filter<'a>(coords: &'a flatbuffers::Vector<'a, flatbuffers::ForwardsUOffset<Coord>>, bbox: [u16; 4]) -> impl Iterator<Item=Coord<'a>> {
+    let min = interleave_morton(bbox[0], bbox[1]);
+    let max = interleave_morton(bbox[2], bbox[3]);
+    let start = bbox_binary_search(&coords, min, 0).unwrap();
+    let end = bbox_binary_search(&coords, max, start).unwrap();
     (start..end).map(move |idx| coords.get(idx as usize))
 }
 
-fn bbox_binary_search(coords: &flatbuffers::Vector<flatbuffers::ForwardsUOffset<Coord>>, pos: u32, offset: u32) -> Result<usize, usize> {
-    let mut size = coords.len();
+fn bbox_binary_search(coords: &flatbuffers::Vector<flatbuffers::ForwardsUOffset<Coord>>, pos: u32, offset: u32) -> Result<u32, u32> {
+    let mut size = coords.len() as u32;
 
     // untested
     if size == 0 {
         return Err(0);
     }
 
-    let mut base = offset as usize;
+    let mut base = offset;
     while size > 1 {
         let half = size / 2;
         let mid = base + half;
-        let v = coords.get(mid).coord();
+            let v = coords.get(mid as usize).coord();
         let cmp = v.cmp(&pos);
         base = if cmp == Greater { base } else { mid };
         size -= half;
     }
-    let cmp = coords.get(base).coord().cmp(&pos);
-    if cmp == Equal { Ok(base) } else { Err(base + (cmp == Less) as usize ) }
+    let cmp = coords.get(base as usize).coord().cmp(&pos);
+    if cmp == Equal { Ok(base) } else { Err(base + (cmp == Less) as u32 ) }
 }
 
 #[cfg(test)]
@@ -63,6 +63,7 @@ mod test {
         let rs = flatbuffers::get_root::<RelevScore>(&data);
 
         let coords = rs.coords().unwrap();
-        let result = bbox_filter(&coords, (0,0,1,1)).collect::<Vec<Coord>>();
+        let result = bbox_filter(&coords, [0,0,1,1]).collect::<Vec<Coord>>();
+        assert_eq!(result.len(), 3);
     }
 }
