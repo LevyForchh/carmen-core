3917d67aafd5a29a2db294654fb5fd9cafa50600
aarthykc <aarthy@mapbox.com>
2019-05-14 06:56:48 -0400

    bbox and proximity filtering

--- rust-src/src/gridstore/spatial.rs
+++ rust-src/src/gridstore/spatial.rs
@@ -9,10 +9,10 @@ use std::cmp::Ordering::{Equal, Greater, Less};
 /// Returns [`Some(Iterator<>`] if the Coord Vector morton order range overlaps with the bouding box,
 /// [`None`] otherwise. May return an Iterator that yields no results if the morton order overlaps
 /// but the actual elements are not in the bounding box.
-pub fn bbox_filter<'a>(
+pub fn bbox_range<'a>(
     coords: flatbuffers::Vector<'a, flatbuffers::ForwardsUOffset<Coord<'a>>>,
     bbox: [u16; 4],
-) -> Option<impl Iterator<Item = Coord<'a>>> {
+) -> Option<(u32, u32)> {
     let min = interleave_morton(bbox[0], bbox[1]);
     let max = interleave_morton(bbox[2], bbox[3]);
     debug_assert!(min <= max, "Invalid bounding box");
@@ -45,8 +45,20 @@ pub fn bbox_filter<'a>(
         end -= 1;
     }
     debug_assert!(start <= end, "Start is before end");
+    Some((start, end))
+}
 
-    Some((start..(end + 1)).filter_map(move |idx| {
+pub fn bbox_filter<'a>(
+    coords: flatbuffers::Vector<'a, flatbuffers::ForwardsUOffset<Coord<'a>>>,
+    bbox: [u16; 4],
+) -> Option<impl Iterator<Item = Coord<'a>>> {
+    let len = coords.len();
+    if len == 0 {
+        return None;
+    }
+
+    let range = bbox_range(coords, bbox)?;
+    Some((range.0..(range.1 + 1)).filter_map(move |idx| {
         let grid = coords.get(idx as usize);
         let (x, y) = deinterleave_morton(grid.coord()); // TODO capture this so we don't have to do it again.
         if x >= bbox[0] && x <= bbox[2] && y >= bbox[1] && y <= bbox[3] {
@@ -83,6 +95,33 @@ pub fn proximity_filter<'a>(
     Some(coord_sets)
 }
 
+pub fn bbox_proximity_filter<'a>(
+    coords: flatbuffers::Vector<'a, flatbuffers::ForwardsUOffset<Coord<'a>>>,
+    bbox: [u16; 4],
+    proximity: (u16, u16),
+) -> Option<impl Iterator<Item = Coord<'a>>> {
+    let range = bbox_range(coords, bbox)?;
+    let prox_pt = interleave_morton(proximity.0, proximity.1) as i64;
+    if coords.len() == 0 {
+        return None;
+    }
+
+    let prox_mid = match coord_binary_search(&coords, prox_pt as u32, 0) {
+        Ok(v) => v,
+        Err(_) => return None,
+    };
+
+    let getter = move |i| coords.get(i as usize);
+    let head = Box::new((range.0..prox_mid).rev().map(getter)) as Box<Iterator<Item = Coord>>;
+    let tail = Box::new((prox_mid..range.1 + 1).map(getter)) as Box<Iterator<Item = Coord>>;
+    let coord_sets = vec![head, tail].into_iter().kmerge_by(move |a, b| {
+        let d1 = (a.coord() as i64 - prox_pt) as i64;
+        let d2 = (b.coord() as i64 - prox_pt) as i64;
+        d1.abs().cmp(&d2.abs()) == Less
+    });
+
+    Some(coord_sets)
+}
 /// Binary search this FlatBuffers Coord Vector
 ///
 /// Derived from binary_search_by in core/slice/mod.rs except this expects descending order.
@@ -267,6 +306,23 @@ mod test {
             vec![7, 6, 8, 13, 1, 21, 24],
             result,
             "sparse result set sorted by z-order in the middle of the result set"
+        );
+    }
+
+    #[test]
+    fn bbox_proximity_search() {
+        let buffer = flatbuffer_generator((1..10).rev()); // [9,8,7,6,5,4,3,2,1]
+        let rs = flatbuffers::get_root::<RelevScore>(&buffer);
+        let coords = rs.coords().unwrap();
+        // bbox is from 1-7; proximity is 4
+        let result = bbox_proximity_filter(coords, [1, 0, 3, 1], (2, 0))
+            .unwrap()
+            .map(|x| x.coord())
+            .collect::<Vec<u32>>();
+        assert_eq!(
+            vec![4, 3, 5, 6, 2, 1, 7],
+            result,
+            "bbox within the range of coordinates; proximity point within the result set"
         );
     }
 
