3cb4276d1e7ceee0ad7bca772194a22c28b5aa25
aarthykc <aarthy@mapbox.com>
2019-06-07 13:32:54 -0400

    make tests pass

--- test/bindings/test.js
+++ test/bindings/test.js
@@ -5,70 +5,70 @@ const tape = require('tape');
 const tmp = require('tmp');
 const rimraf = require('rimraf').sync;
 
-tape('JsGridbuilderBuilder init', (t) => {
+tape('JsGridStoreBuilder init', (t) => {
     const tmpDir = tmp.dirSync();
-    t.throws(() => new addon.GridbuilderBuilder(), 'not enough arguments');
-    t.throws(() => new addon.GridbuilderBuilder({}), 'throws on wrong argument type');
-    t.throws(() => new addon.GridbuilderBuilder(7), 'throws on wrong argument type');
-    const builder = new addon.GridbuilderBuilder(tmpDir.name);
+    t.throws(() => new addon.GridStoreBuilder(), 'not enough arguments');
+    t.throws(() => new addon.GridStoreBuilder({}), 'throws on wrong argument type');
+    t.throws(() => new addon.GridStoreBuilder(7), 'throws on wrong argument type');
+    const builder = new addon.GridStoreBuilder(tmpDir.name);
     t.ok(builder);
     rimraf(tmpDir.name);
     t.end();
 });
 
-tape('GridbuilderBuilder insert()', (t) => {
+tape('GridStoreBuilder insert()', (t) => {
     const tmpDir = tmp.dirSync();
-    const builder = new addon.GridbuilderBuilder(tmpDir.name);
+    const builder = new addon.GridStoreBuilder(tmpDir.name);
     t.throws(() => builder.insert(), 'not enough arguments');
     t.throws(() => builder.insert({}), 'not enough arguments');
     builder.insert({ phrase_id: 0, lang_set: [0] }, [{ id: 0, x: 0, y: 0, relev: 0.5, score: 2, source_phrase_hash: 0 }]);
     builder.insert({ phrase_id: 1, lang_set: [0, 1, 2, 3] }, [{ id: 2, x: 2, y: 2, relev: 0.6, score: 3, source_phrase_hash: 0 }]);
     builder.finish();
 
-    const reader = new addon.Gridbuilder(tmpDir.name);
+    const reader = new addon.GridStore(tmpDir.name);
     t.deepEquals(reader.get({ phrase_id: 0, lang_set: [0] }), [ { relev: 1, score: 2, x: 0, y: 0, id: 0, source_phrase_hash: 0 } ], 'able to find the correct gridEntry inserted by insert()');
     t.deepEquals(reader.get({ phrase_id: 1, lang_set: [0, 1, 2, 3] }), [ { relev: 0.6, score: 3, x: 2, y: 2, id: 2, source_phrase_hash: 0 } ], 'able to find the correct gridEntry inserted by insert()');
     t.notOk(reader.get({ phrase_id: 3, lang_set: [3] }), 'cannot retrieve a grid that has not been inserted');
     t.end();
 });
 
-tape('GridbuilderBuilder append()', (t) => {
+tape('GridStoreBuilder append()', (t) => {
     const tmpDir = tmp.dirSync();
-    const builder = new addon.GridbuilderBuilder(tmpDir.name);
+    const builder = new addon.GridStoreBuilder(tmpDir.name);
     t.throws(() => builder.append(), 'not enough arguments');
     t.throws(() => builder.append({}), 'not enough arguments');
     builder.insert({ phrase_id: 0, lang_set: [0] }, [{ id: 0, x: 0, y: 0, relev: 0.5, score: 2, source_phrase_hash: 0 }]);
     builder.append({ phrase_id: 1, lang_set: [0, 2] }, [{ id: 2, x: 2, y: 2, relev: 0.8, score: 3, source_phrase_hash: 0 }]);
     builder.finish();
 
-    const reader = new addon.Gridbuilder(tmpDir.name);
+    const reader = new addon.GridStore(tmpDir.name);
     let list = Array.from(reader.keys());
-    t.deepEquals(list, [ { phrase_id: 0, lang_set: [ 0 ] }, { phrase_id: 1, lang_set: [ 0, 2 ] } ], 'Gridbuilder contains the key inserted and the key appended');
+    t.deepEquals(list, [ { phrase_id: 0, lang_set: [ 0 ] }, { phrase_id: 1, lang_set: [ 0, 2 ] } ], 'GridStore contains the key inserted and the key appended');
     t.end();
 });
 
-tape('GridbuilderBuilder finish()', (t) => {
+tape('GridStoreBuilder finish()', (t) => {
     const tmpDir = tmp.dirSync();
-    const builder = new addon.GridbuilderBuilder(tmpDir.name);
+    const builder = new addon.GridStoreBuilder(tmpDir.name);
     builder.insert({ phrase_id: 0, lang_set: [0] }, [{ id: 0, x: 0, y: 0, relev: 0.5, score: 2, source_phrase_hash: 0 }]);
     builder.insert({ phrase_id: 1, lang_set: [0, 1, 2, 3] }, [{ id: 2, x: 2, y: 2, relev: 0.8, score: 3, source_phrase_hash: 0 }]);
-    t.throws(() => new addon.Gridbuilder(tmpDir.name), 'throws if you attempt to read without calling finish()');
+    t.throws(() => new addon.GridStore(tmpDir.name), 'throws if you attempt to read without calling finish()');
 
     builder.finish();
-    const reader = new addon.Gridbuilder(tmpDir.name);
+    const reader = new addon.GridStore(tmpDir.name);
     t.ok(reader, 'can read only after finish() is called');
     t.end();
 });
 
-tape('Gridbuilder reader', (t) => {
+tape('GridStore reader', (t) => {
     const tmpDir = tmp.dirSync();
-    const builder = new addon.GridbuilderBuilder(tmpDir.name);
+    const builder = new addon.GridStoreBuilder(tmpDir.name);
     builder.insert({ phrase_id: 0, lang_set: [0] }, [{ id: 0, x: 0, y: 0, relev: 0.5, score: 2, source_phrase_hash: 0 }]);
     builder.insert({ phrase_id: 1, lang_set: [0, 1, 2, 3] }, [{ id: 2, x: 2, y: 2, relev: 0.8, score: 3, source_phrase_hash: 0 }]);
     builder.finish();
 
-    const reader = new addon.Gridbuilder(tmpDir.name);
+    const reader = new addon.GridStore(tmpDir.name);
     let list = Array.from(reader.keys());
-    t.deepEquals(list, [ { phrase_id: 0, lang_set: [ 0 ] }, { phrase_id: 1, lang_set: [ 0, 1, 2, 3 ] } ], 'Gridbuilder is able to retrieve keys, reader works as expected');
+    t.deepEquals(list, [ { phrase_id: 0, lang_set: [ 0 ] }, { phrase_id: 1, lang_set: [ 0, 1, 2, 3 ] } ], 'GridStore is able to retrieve keys, reader works as expected');
     t.end();
 });
