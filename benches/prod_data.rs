55dc634f94fd91683ba0780977a6e6c2d5207cc0
Andrew Pendleton <andrew@mapbox.com>
2019-08-05 14:55:03 -0400

    Add benchmark for insertion, plus clean up warnings

--- benches/prod_data.rs
+++ benches/prod_data.rs
@@ -1,8 +1,11 @@
-use std::borrow::Borrow;
-use std::path::Path;
-use std::rc::Rc;
+use std::fs::File;
+use std::io::{self, BufRead};
 
-use criterion::{black_box, Bencher, Criterion, Fun};
+use criterion::{Bencher, Criterion, Fun};
+use lz4::Decoder;
+use once_cell::unsync::Lazy;
+use serde_json;
+use tempfile;
 
 use carmen_core::gridstore::*;
 use test_utils::*;
@@ -17,7 +20,7 @@ pub fn benchmark(c: &mut Criterion) {
 
         b.iter(|| {
             let (stack, opts) = cycle.next().unwrap();
-            coalesce(stack.clone(), opts);
+            coalesce(stack.clone(), opts).unwrap();
         })
     }));
 
@@ -28,7 +31,7 @@ pub fn benchmark(c: &mut Criterion) {
 
         b.iter(|| {
             let (stack, opts) = cycle.next().unwrap();
-            coalesce(stack.clone(), opts);
+            coalesce(stack.clone(), opts).unwrap();
         })
     }));
 
@@ -39,7 +42,7 @@ pub fn benchmark(c: &mut Criterion) {
 
         b.iter(|| {
             let (stack, opts) = cycle.next().unwrap();
-            coalesce(stack.clone(), opts);
+            coalesce(stack.clone(), opts).unwrap();
         })
     }));
 
@@ -50,7 +53,41 @@ pub fn benchmark(c: &mut Criterion) {
 
         b.iter(|| {
             let (stack, opts) = cycle.next().unwrap();
-            coalesce(stack.clone(), opts);
+            coalesce(stack.clone(), opts).unwrap();
+        })
+    }));
+
+    let eur_records = Lazy::new(|| {
+        let dl_path = ensure_downloaded("europen-place-both-740ed51f45-d775d2eb65.gridstore.dat.lz4");
+        let decoder = Decoder::new(File::open(dl_path).unwrap()).unwrap();
+        let file = io::BufReader::new(decoder);
+
+        let records: Vec<StoreEntryBuildingBlock> = file.lines().filter_map(|l| {
+            let record = l.unwrap();
+            if record.is_empty() {
+                None
+            } else {
+                Some(serde_json::from_str(&record).unwrap())
+            }
+        }).collect();
+        records
+    });
+    to_bench.push(Fun::new("builder_insert", move |b: &mut Bencher, _i| {
+        let mut dir: Option<tempfile::TempDir> = None;
+        let mut builder: Option<GridStoreBuilder> = None;
+        let mut i = 0;
+
+        b.iter(|| {
+            if i == 0 {
+                // every time we're at the beginning of the list, start a new builder
+                // and throw away the old one
+                dir.replace(tempfile::tempdir().unwrap());
+                builder.replace(GridStoreBuilder::new(dir.as_mut().unwrap().path()).unwrap());
+            }
+            let record = &eur_records[i];
+            builder.as_mut().unwrap().insert(&record.grid_key, &record.entries).unwrap();
+
+            i = (i + 1) % (eur_records.len());
         })
     }));
 
