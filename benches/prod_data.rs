4dee1199cb5c8bc7c300d5234bfadb8ee263f55f
Andrew Pendleton <andrew@mapbox.com>
2019-08-05 13:52:35 -0400

    Autocomplete coalesce benches

--- benches/prod_data.rs
+++ benches/prod_data.rs
@@ -32,5 +32,27 @@ pub fn benchmark(c: &mut Criterion) {
         })
     }));
 
+    to_bench.push(Fun::new("coalesce_ac_global", move |b: &mut Bencher, _i| {
+        let stacks = prepare_coalesce_stacks("gb_address_ac_global.ljson.lz4");
+
+        let mut cycle = stacks.iter().cycle();
+
+        b.iter(|| {
+            let (stack, opts) = cycle.next().unwrap();
+            coalesce(stack.clone(), opts);
+        })
+    }));
+
+    to_bench.push(Fun::new("coalesce_ac_prox", move |b: &mut Bencher, _i| {
+        let stacks = prepare_coalesce_stacks("gb_address_ac_with_proximity.ljson.lz4");
+
+        let mut cycle = stacks.iter().cycle();
+
+        b.iter(|| {
+            let (stack, opts) = cycle.next().unwrap();
+            coalesce(stack.clone(), opts);
+        })
+    }));
+
     c.bench_functions("prod_data", to_bench, ());
 }
