b7d47a477c5bba7ff6bbed6a20a1f2bc27885692
Andrew Pendleton <andrew@mapbox.com>
2019-08-06 15:42:06 -0400

    Add asi test from real indexing capture

--- benches/prod_data.rs
+++ benches/prod_data.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
-use criterion::{Bencher, Criterion, Fun};
+use criterion::{Bencher, Criterion, Fun, BatchSize};
 use lz4::Decoder;
 use once_cell::unsync::Lazy;
 use serde_json;
@@ -73,6 +73,8 @@ pub fn benchmark(c: &mut Criterion) {
         records
     });
     to_bench.push(Fun::new("builder_insert", move |b: &mut Bencher, _i| {
+        Lazy::force(&eur_records);
+
         let mut dir: Option<tempfile::TempDir> = None;
         let mut builder: Option<GridStoreBuilder> = None;
         let mut i = 0;
@@ -89,6 +91,42 @@ pub fn benchmark(c: &mut Criterion) {
 
             i = (i + 1) % (eur_records.len());
         })
+    }));
+
+    let asi_records = Lazy::new(|| {
+        let dl_path = ensure_downloaded("asiopen_place_appends.ljson.lz4");
+        let decoder = Decoder::new(File::open(dl_path).unwrap()).unwrap();
+        let file = io::BufReader::new(decoder);
+
+        let records: Vec<(GridKey, Vec<GridEntry>)> = file.lines().filter_map(|l| {
+            let record = l.unwrap();
+            if record.is_empty() {
+                None
+            } else {
+                Some(serde_json::from_str(&record).unwrap())
+            }
+        }).collect();
+        records
+    });
+    to_bench.push(Fun::new("builder_append_logged", move |b: &mut Bencher, _i| {
+        Lazy::force(&asi_records);
+
+        let mut dir: Option<tempfile::TempDir> = None;
+        let mut builder: Option<GridStoreBuilder> = None;
+        let mut i = 0;
+
+        b.iter_batched(|| {}, |_| {
+            if i == 0 {
+                // every time we're at the beginning of the list, start a new builder
+                // and throw away the old one
+                dir.replace(tempfile::tempdir().unwrap());
+                builder.replace(GridStoreBuilder::new(dir.as_mut().unwrap().path()).unwrap());
+            }
+            let record = &asi_records[i];
+            builder.as_mut().unwrap().insert(&record.0, &record.1).unwrap();
+
+            i = (i + 1) % (asi_records.len());
+        }, BatchSize::NumIterations(100_000))
     }));
 
     c.bench_functions("prod_data", to_bench, ());
