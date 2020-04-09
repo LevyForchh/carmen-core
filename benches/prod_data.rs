a741668fd1fcc23b68205ad90fa51d01987c40c7
Andrew Pendleton <andrew@mapbox.com>
2020-04-09 18:03:27 -0400

    Fix formatting in benchmarks

--- benches/prod_data.rs
+++ benches/prod_data.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-use criterion::{Bencher, Criterion, Benchmark};
+use criterion::{Bencher, Benchmark, Criterion};
 
 use carmen_core::gridstore::*;
 use test_utils::*;
@@ -10,26 +10,29 @@ pub fn benchmark(c: &mut Criterion) {
         ("coalesce_global", "gb_address_pm_global.ljson.lz4"),
         ("coalesce_prox", "gb_address_pm_with_proximity.ljson.lz4"),
         ("coalesce_ac_global", "gb_address_pm_ac_global.ljson.lz4"),
-        ("coalesce_ac_prox", "gb_address_pm_ac_with_proximity.ljson.lz4")
+        ("coalesce_ac_prox", "gb_address_pm_ac_with_proximity.ljson.lz4"),
     ];
 
     for (label, file) in to_bench {
         c.bench(
             label,
-            Benchmark::new(
-                label,
-                move |b: &mut Bencher| {
-                    let queries = prepare_phrasematches(file);
-                    let trees: Vec<_> = queries.into_iter().map(|(query, opts)| (stackable(&query, None, 0, HashSet::new(), 0, 129, 0.0, 0), opts)).collect();
-
-                    let mut cycle = trees.iter().cycle();
-
-                    b.iter(|| {
-                        let (tree, opts) = cycle.next().unwrap();
-                        tree_coalesce(&tree, &opts).unwrap()
+            Benchmark::new(label, move |b: &mut Bencher| {
+                let queries = prepare_phrasematches(file);
+                let trees: Vec<_> = queries
+                    .into_iter()
+                    .map(|(query, opts)| {
+                        (stackable(&query, None, 0, HashSet::new(), 0, 129, 0.0, 0), opts)
                     })
-                }
-            ).sample_size(20)
+                    .collect();
+
+                let mut cycle = trees.iter().cycle();
+
+                b.iter(|| {
+                    let (tree, opts) = cycle.next().unwrap();
+                    tree_coalesce(&tree, &opts).unwrap()
+                })
+            })
+            .sample_size(20),
         );
     }
 }
